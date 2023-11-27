c927a6fccc852bd78c63c0f461e03037bb2c4be8
Frank Lehmann <franksimonlehmann@hotmail.com>
2023-11-27 16:50:27 +0100

    created booking model

---
+++ db/migrate/20231127154849_create_bookings.rb
@@ -0,0 +1,14 @@
+class CreateBookings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :star, null: false, foreign_key: true
+      t.boolean :booking_status
+      t.text :comment
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
