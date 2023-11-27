fa198e5b2f1d0a52d1258fc905c0138061b3a2ba
Usman Ali <usmanv5@hotmail.com>
2023-11-27 16:16:42 +0000

    some star validation

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,3 +1,12 @@
 class Star < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  validates :name, presense: true
+  validates :description, presense: true
+  validates :description, length: { minimum: 5 }
+  validates :integer, presense: true
+  validates :price, presense: true
+  validates :boolean, presense: true
+  validates :location, presense: true
 end
