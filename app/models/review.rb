76216fa46cca95aea9dcdf4055ef6c228d0fbfc3
Usman Ali <usmanv5@hotmail.com>
2023-11-27 16:17:27 +0000

    rating validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  validates :comment, :rating, presense: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :comment, length: { minimum: 5 }
 end
