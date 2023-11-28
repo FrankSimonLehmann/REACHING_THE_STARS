ce6f9891261aa6aa76ca9d24392fa7080b2166aa
Usman Ali <usmanv5@hotmail.com>
2023-11-28 07:55:25 +0000

    fixed stars a bit

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,7 +1,7 @@
 class Star < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, through: :bookings, dependent: :destroy
   validates :name, presence: true
   validates :description, presence: true
   validates :description, length: { minimum: 5 }
