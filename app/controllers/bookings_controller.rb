365227c51ec08686bcb1e64ec655050e2d9f0716
theresa schottl <theresaschoettl1@gmail.com>
2023-11-28 16:46:26 +0100

    created the bookings controller, changed the default of the booking status to false

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,48 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def show
+    @booking = Booking.all
+  end
+
+  # def edit
+  #   @booking = Booking.find(params[:id])
+  # end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    redirect_to stars_path(@stars)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    if @booking.save
+      redirect_to stars_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def accept
+    if @booking.accepted!
+      redirect_to @booking, notice: "Booking accepted"
+    else
+      redirect_to @booking, notice: "Booking could not be accepted, please try again"
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to stars_path(@booking.user), status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:booking_status, :comment, :start_date, :end_date)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,12 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :star
   has_many :reviews, dependent: :destroy
+
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  def accepted!
+    booking.accepted = true
+  end
+
 end
