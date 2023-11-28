1eab51c66929e9a92dc460f58dba13a6e50607b8
Usman Ali <usmanv5@hotmail.com>
2023-11-28 13:57:55 +0000

    pages done, devise done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,12 @@ class PagesController < ApplicationController
   def home
     @stars = Star.all
   end
+
+  def booking
+    @bookings = Booking.all.select { |i| i.user_id == current_user.id }
+  end
+
+  def ownedstars
+    @stars = Star.all.select { |i| i.user_id == current_user.id }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "home", to: "pages#home"
+  get "booking", to: "pages#booking"
+  get "ownedstars", to: "pages#ownedstars"
   # Defines the root path route ("/")
   # root "posts#index"
 end
