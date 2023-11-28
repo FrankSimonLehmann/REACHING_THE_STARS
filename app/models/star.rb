a645ca51abad176425adfd6c357b3951c1e97fdb
Usman Ali <usmanv5@hotmail.com>
2023-11-28 11:40:29 +0000

    fixed validations a bit more, made the db seeds and tested each model

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,7 +1,7 @@
 class Star < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  has_many :reviews, through: :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
   validates :name, presence: true
   validates :description, presence: true
   validates :description, length: { minimum: 5 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "destroying previous seeds"
+Star.destroy_all
+User.destroy_all
+Booking.destroy_all
+Review.destroy_all
+puts "destroyed seeds"
+user = User.create!(email: "usmanv5@hotmail.com", password: "password", first_name: "usman" , last_name: "ali", address: "pluto", user_description: "hello world")
+star = Star.create!(user: user, name: "bob", description: "epic star", registration_number: 420, price: 420.420, availability: true, location: "in narnia")
+booking = Booking.create!(user: user, star: star, booking_status: false, comment: 'hello world this is my comment', start_date: Time.now, end_date: Time.new(2023,12,12))
+Review.create!(comment: "hahaaaaaaaaaaaaaaaaaaaa im dumbs", rating: 3, booking: booking)
